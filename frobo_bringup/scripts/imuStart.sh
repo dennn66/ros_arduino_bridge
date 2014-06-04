92f5d0a6a7142de5a14c180bc4aef7b8ef80a0b1
dennn66 <dennn6696@gmail.com>
2014-06-05 02:48:30 +0600

    ...

---
+++ frobo_bringup/scripts/imuStart.sh
@@ -0,0 +1,1 @@
+roslaunch razor_imu_9dof razor-pub.launch device_port:=/dev/arduino_imu
---
+++ frobo_bringup/scripts/laserStart.sh
@@ -0,0 +1,1 @@
+rosrun xv_11_laser_driver neato_laser_publisher _port:=/dev/lidar _firmware_version:=2 _frame_id:=/lidar_link
